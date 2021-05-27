b1af5bf7bde6f09665e9ccfe5c9bb4e72e48bfcc
Denis_Lobanov_RL6-29 <80922636+DenisOffor@users.noreply.github.com>
2021-05-27 19:22:19 +0300

    Update _15_zadacha.cpp

--- _15_zadacha.cpp
+++ _15_zadacha.cpp
@@ -3,13 +3,25 @@
 //Написать функцию, которой на вход подаётся одномерный массив и число n (может быть положительным, или отрицательным), при этом метод должен циклически сместить все элементы массива на n позиций.
 
 void changeShift(int* arr, int size, int shift){
+  if(size <= 1)
+    return;
   int temp = 0;
-  while(shift--){
-      temp = arr[size - 1];
-      if(size > 0)
-        for (int i = size -1; i > 0; --i){
-            arr[i] = arr[i - 1];
-          }
-      arr[0] = temp;
+  if(shift < 0){
+      while(shift++){
+          temp = arr[0];
+            for (int i = 0; i < size - 1; i++){
+                arr[i] = arr[i + 1];
+              }
+          arr[size - 1] = temp;
+        }
+    }
+  else{
+      while(shift--){
+          temp = arr[size - 1];
+            for (int i = size -1; i > 0; --i){
+                arr[i] = arr[i - 1];
+              }
+          arr[0] = temp;
+        }
     }
 }
